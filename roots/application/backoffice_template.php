c94a4d03117e704c629824e8b6cd08ad6fb471ae
Joshua Washburn <github@electr.us>
2022-05-22 23:15:47 -0400

    Switched to View/Controller model. Added config.

---
+++ roots/application/backoffice_template.php
@@ -0,0 +1,27 @@
+<html>
+        <head>
+                <title>$$$ - Inventory System</title>
+		<link rel="stylesheet" href="style.php" type="text/css"></style>
+        </head>
+        <body>
+		<div id="heading">
+			<img id="logo"/>
+			<ul id="menu">
+				<li><a href="./">Dashboard</a></li>
+			</ul>
+		</div>
+		<div id="page">
+			<div class="ui-fade" id="uif-h"></div>
+			<div id="content">
+<?php
+	print $contents;
+?>
+			</div>
+			<div class="ui-fade" id="uif-f"></div>
+		</div>
+		<div id="footing">
+			<div id="copyright">Sample application utilizing MariaDB, PHP-FPM, and Nginx developed by Joshua Washburn during May 2022</div>
+		</div>
+        </body>
+</html>
+
--- roots/web_root/backoffice/index.php
+++ roots/web_root/backoffice/index.php
@@ -1,28 +1,9 @@
-<php
-include "../../../application/config.phdp" or die "Can't find config";
-?><html>
-        <head>
-                <title>$$$ - Inventory System</title>
-		<link rel="stylesheet" href="style.php" type="text/css"></style>
-        </head>
-        <body>
-		<div id="heading">
-			<img id="logo"/>
-			<ul id="menu">
-				<li><a href="./">Dashboard</a></li>
-			</ul>
-		</div>
-		<div id="page">
-			<div class="ui-fade" id="uif-h"></div>
-			<div id="content">
-				<h1>test</h1>
-				<div>test</div>
-			</div>
-			<div class="ui-fade" id="uif-f"></div>
-		</div>
-		<div id="footing">
-			<div id="copyright">Sample application utilizing MariaDB, PHP-FPM, and Nginx developed by Joshua Washburn during May 2022</div>
-		</div>
-        </body>
-</html>
-
+<?php
+require "../../../conf/app.conf.php";
+?><h1>test</h1>
+<div>test</div>
+<?php
+$contents = ob_get_contents();
+ob_end_clean();
+require "../../application/backoffice_template.php";
+?>
