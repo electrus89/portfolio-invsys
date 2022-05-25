ba8d84e43a84233785f52cb3b4dc1d9cb3dbc1da
Joshua Washburn <github@electr.us>
2022-05-25 11:09:08 -0400

    Update create_database.sql
    Fixed the creation SQL file; added CREATE USER, USE DATABASE, and GRANT commands.

--- install/create_database.sql
+++ install/create_database.sql
@@ -1,38 +1,12 @@
 CREATE DATABASE invsys;
 CREATE USER 'invsys'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL on invsys.* to invsys@localhost;
 
-CREATE TABLE items (
-	ID		INT AUTO_INCREMENT PRIMARY KEY,
-	ShortName	VARCHAR(255),
-	Description	TEXT
-);
-CREATE TABLE assignments (
-	ID		INT AUTO_INCREMENT PRIMARY KEY,
-	ItemID		INT,
-	AssignedTo	INT,
-	AssignedBy	INT,
-	Status		INT
-)
-CREATE TABLE entities (
-	ID		INT AUTO_INCREMENT PRIMARY KEY,
-	FullName	VARCHAR(255),
-	Description	TEXT,
-	Classification	INT
-)
-CREATE TABLE entity_to_user (
-	ID		INT AUTO_INCREMENT PRIMARY KEY,
-	EntityID	INT,
-	Username	VARCHAR(20)
-)
-CREATE TABLE credentials (
-	ID		INT AUTO_INCREMENT PRIMARY KEY,
-	UserID		INT,
-	Credential	VARCHAR(255)
-	CredType INT
-)
-CREATE TABLE entity_relationships (
-	ID		INT AUTO_INCREMENT PRIMARY KEY,
-	EntityMaster	INT,
-	EntitySub	INT,
-	Relationship	INT
-)
+USE DATABASE invsys;
+
+CREATE TABLE items ( ID INT AUTO_INCREMENT PRIMARY KEY, ShortName VARCHAR(255), Description TEXT);
+CREATE TABLE assignments ( ID INT AUTO_INCREMENT PRIMARY KEY, ItemID INT, AssignedTo INT, AssignedBy INT, Status INT);
+CREATE TABLE entities ( ID INT AUTO_INCREMENT PRIMARY KEY, FullName VARCHAR(255), Description TEXT, Classification INT);
+CREATE TABLE entity_to_user ( ID INT AUTO_INCREMENT PRIMARY KEY, EntityID INT, Username TEXT);
+CREATE TABLE credentials ( ID INT AUTO_INCREMENT PRIMARY KEY, UserID INT, Credential VARCHAR(255), CredType INT);
+CREATE TABLE entity_relationships ( ID INT AUTO_INCREMENT PRIMARY KEY,  EntityMaster INT,  EntitySub INT, Relationship INT);
