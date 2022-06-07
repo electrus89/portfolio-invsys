072d3de2e0fd26209216ef49450f031c44850542
Joshua Washburn <github@electr.us>
2022-06-07 11:57:51 -0400

    Update controller2.php
    Partial addition of Select Queries in MySQL class.

--- roots/application/controller2.php
+++ roots/application/controller2.php
@@ -15,6 +15,38 @@ class QueryResult
 	public $data;				// For select queries, the data selected.
 }
 
+class SelectQuery
+{
+	public $fields = array();
+	public $tables = array();
+	public $orderby = array("field"=>"",
+							"direction"=>"");
+	public $groupby = array("field"=>"");
+	public $where = array();
+	public $limit = array("offset" => 0,
+						  "quantity" => 0);
+						  
+	public function __toString()
+	{
+		$fields = implode(",", $this->fields);
+		$tables = implode(" INNER JOIN ", $this->tables);
+		if ($this->orderby["field"] === "") {
+			$orderby = "";
+		} elseif (($this->orderby["field"] !== "") && ($this->orderby["direction"] === "")) {
+			$orderby = " ORDER BY {$this->orderby['field']}";
+		} else {
+			$orderby = " ORDER BY {$this->orderby['field']} {$this->orderby['direction']}";
+		}
+		$limit = " LIMIT {$this->limit['quantity']}, {$this->limit['offset']}";
+		if (count($this->where) != 0) {
+			$where = " WHERE ".implode(" AND ", $this->where);
+		} else {
+			$where = "";
+		}
+		return "SELECT {$fields} FROM {$tables}{$where}{$orderby}{$limit}"; 
+	}
+}
+
 class MySQLDatabase extends Database
 {
 	private $conn;			// This is the connection object.
@@ -38,6 +70,31 @@ class MySQLDatabase extends Database
 	
     function Select(SelectQuery $query) : QueryResult
 	{
+		/*
+		$result = $this->innerMysqli->query($q = "SELECT {$qfields} FROM {$table} {$qlimits} {$qrange} {$qgroupby}");
+		
+		// Let everyone know if this went wrong.
+		if ($this->innerMysqli->errno != 0) return null;
+		
+		$columns = array();
+		
+		$assocdata = $result->fetch_all(MYSQLI_ASSOC);
+		if (($qfields == "*") && ($assocdata !== array()))
+		{
+			// If we chose all fields, we don't have a handy list already...
+			$columns = array_keys($assocdata[0]);
+		} elseif ($qfields == "*") {
+			// Welp. We tried.
+			$columns = array("NODATA");
+		} else {
+			// Oh! We have a list already. Let's use that...
+			$columns = $fields;
+		}
+		return array (
+						"query" => $q,
+						"columns" => $columns,
+						"data" => $assocdata
+					);*/
 	}
 	
 	function Update(UpdateQuery $query) : QueryResult
